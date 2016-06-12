3953eac569d5309f8cfa891cd037b53e7157d5a9
ragozin-n <lastjanuary@yandex.ru>
2016-06-12 18:18:37 +0500

    IndexDay optimization

--- DATAO/AdminForm.cs
+++ DATAO/AdminForm.cs
@@ -170,26 +170,27 @@ namespace DATAO
 
         private int IndexDay()
         {
-            //твой ретурн инт возращал не правильные индексы!
+            // Американская неделя начинается с Воскресения
+            return (monthCalendar.SelectionStart.DayOfWeek == DayOfWeek.Sunday ? 6 : (int)monthCalendar.SelectionStart.DayOfWeek - 1);
 
-            switch (monthCalendar.SelectionStart.DayOfWeek)
-            {
-                case DayOfWeek.Monday:
-                    return 0;
-                case DayOfWeek.Tuesday:
-                    return 1;
-                case DayOfWeek.Wednesday:
-                    return 2;
-                case DayOfWeek.Thursday:
-                    return 3;
-                case DayOfWeek.Friday:
-                    return 4;
-                case DayOfWeek.Saturday:
-                    return 5;
-                case DayOfWeek.Sunday:
-                    return 6;
-            }
-            return 0;
+            //switch (monthCalendar.SelectionStart.DayOfWeek)
+            //{
+            //    case DayOfWeek.Monday:
+            //        return 0;
+            //    case DayOfWeek.Tuesday:
+            //        return 1;
+            //    case DayOfWeek.Wednesday:
+            //        return 2;
+            //    case DayOfWeek.Thursday:
+            //        return 3;
+            //    case DayOfWeek.Friday:
+            //        return 4;
+            //    case DayOfWeek.Saturday:
+            //        return 5;
+            //    case DayOfWeek.Sunday:
+            //        return 6;
+            //}
+            //return 0;
         }
 
         private void LoadSchedule()
